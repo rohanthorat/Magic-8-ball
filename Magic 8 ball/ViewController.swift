2753428dbd18655822f3b8e1602f279c74574b48
Rohan Thorat <rthorat1@binghamton.edu>
2018-05-23 22:15:08 -0400

    adding label-imageview-button

---
+++ Magic 8 ball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Magic 8 ball
+//
+//  Created by Rohan Thorat on 5/23/18.
+//  Copyright © 2018 Rohan Thorat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
